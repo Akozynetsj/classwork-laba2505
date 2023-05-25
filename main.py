b2f57003343c79521271eb5a426b56466a9f7a9f
student <Kozin_dn78@student.itstep.org>
2023-05-25 19:37:34 +0300

    bs4

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ print(response.read())
 
 import requests
 response = requests.get("https://coinmarketcap.com/")
-print(response.text)
+'''print(response.text)
+response_parse = response.text.split('<span>')
+for elem in response_parse:
+    if  elem.startswith('$'):
+        print(elem)'''
+from bs4 import BeautifulSoup
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text,features= 'html.parser')
+    soup_list = soup.find_all('a', {'href': '/currencies/bitcoin/markets/'})
+    res = soup_list[0].find('span')
+    print(res.text)
